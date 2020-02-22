960c1064bb9bfd104cc0805f7fde26b685d14dcf
554518983 <ZH23>
2020-02-22 13:58:42 +0800

    项目结构

---
+++ Web/SpringBoot/ConfigurationFile/src/main/java/com/zjb/configurationfile/ConfigurationFileApplication.java
@@ -0,0 +1,15 @@
+package com.zjb.configurationfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaojianbo
+ * @date 2020/2/22 13:57
+ */
+@SpringBootApplication
+public class ConfigurationFileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationFileApplication.class);
+    }
+}
